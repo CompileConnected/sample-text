cf89fc565a53c5fa58e7d9adf27be396b0537960
Yohanes <yokho95@gmail.com>
2020-08-22 17:11:16 +0700

    add product table

---
+++ src/views/product/Product.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <CRow>
+      <CCol lg="6">
+        <ProductTable :items="getShuffledUsersData()">
+          <template #header>
+            <CIcon name="cil-grid" /> Product Table
+          </template>
+        </ProductTable>
+      </CCol>
+    </CRow>
+  </div>
+</template>
+
+<script>
+import ProductTable from "./ProductTable";
+import productMockData from './ProductMockData';
+
+export default {
+  name: "Product",
+  components: { ProductTable },
+  methods: {
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return array;
+    },
+
+    getShuffledUsersData() {
+      return this.shuffleArray(productMockData.slice(0));
+    },
+  },
+};
+</script>
---
+++ src/views/product/ProductTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <CCard>
+    <CCardHeader>
+      <slot name="header"> <CIcon name="cil-grid" /> {{ caption }} </slot>
+    </CCardHeader>
+    <CCardBody>
+      <CDataTable
+        :hover="hover"
+        :striped="striped"
+        :bordered="bordered"
+        :small="small"
+        :fixed="fixed"
+        :items="items"
+        :fields="fields"
+        :items-per-page="small ? 10 : 5"
+        :dark="dark"
+        pagination
+      >
+      </CDataTable>
+    </CCardBody>
+  </CCard>
+</template>
+
+<script>
+export default {
+  name: "ProductTable",
+  props: {
+    items: Array,
+    fields: {
+      type: Array,
+      default() {
+        return ["id","name", "price", "actual_quantity", "reserve_quantity", "purchased_quantity"];
+      },
+    },
+    caption: {
+      type: String,
+      default: "Table",
+    },
+    hover: Boolean,
+    striped: Boolean,
+    bordered: Boolean,
+    small: Boolean,
+    fixed: Boolean,
+    dark: Boolean,
+  },
+};
+</script>
