3aba1d67aa840285978aa94a439a7211e9125600
Yohanes <yokho95@gmail.com>
2020-08-30 14:51:50 +0700

    add internalization

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
+
   transpileDependencies: [
     '@coreui/utils'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'id',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
